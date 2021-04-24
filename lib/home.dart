7a2a2c7969007459d30b78e33529257ec931e1d5
rexsolis7458 <rexbryan.solis.20@usjr.edu.ph>
2021-04-24 10:05:13 +0800

    splashcreen

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 3,
+      backgroundColor: Colors.white,
+      image: Image.asset('images/loading.gif'),
+      loaderColor: Colors.white,
+      photoSize: 150.0,
+    );
+  }
+}
