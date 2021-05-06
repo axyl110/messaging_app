d7c80da09d2d68670b5e664ece423e336f3949fe
axyl110 <axylvillanueva599@gmail.com>
2021-05-06 19:37:27 +0800

    database

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
-import 'package:messaging_app/home.dart';
+import 'package:messaging_app/helper/authenticate.dart';
+//import 'views/signin.dart';
+// import 'views/signUp.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,25 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SplashScreenPage(),
+          primaryColor: Color(0xff145C9E),
+          scaffoldBackgroundColor: Color(0xff1F1F1F)),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class SplashScreenPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 5,
-      navigateAfterSeconds: new HomeScreen(),
-      backgroundColor: Colors.blue[100],
-      title: Text('Message App'),
-      image: Image.asset('images/load.gif'),
-      loaderColor: Colors.white,
+      home: Authenticate(),
     );
   }
 }
